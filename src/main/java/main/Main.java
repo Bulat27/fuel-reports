fa9a0e8ffb74ae40f16cb0180be793514b97b87a
Nikola Bulat <nbulat99@gmail.com>
2021-07-21 15:27:00 +0200

    CLI - Logger Thread Resolved

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,7 @@
 package main;
 
 import cli.view_models.ConfigCommand;
-import cli.view_models.FuelReportsArgs;
+//import cli.view_models.FuelReportsArgs;
 import cli.view_models.ProcessCommand;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
@@ -12,14 +12,20 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
+    
+   private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) throws SQLException {
         JCommander jc = null;
+        LOGGER.addHandler(new ConsoleHandler());
 
         try {
-            FuelReportsArgs mainCommand = new FuelReportsArgs();
+            //FuelReportsArgs mainCommand = new FuelReportsArgs();
             ProcessCommand processCommand = new ProcessCommand();
             ConfigCommand configCommand = new ConfigCommand();
 
@@ -35,14 +41,12 @@ public class Main {
                     processCommand.process(configCommand.getDestinationDir());
                     break;
                 default:
-                    System.err.println("Invalid command " + parsedCmdStr);
+                    LOGGER.log(Level.FINE, "Invalid command : {0}", parsedCmdStr);
             }
         } catch (JSchException | SAXException | ParserConfigurationException | IOException | SftpException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, e.toString(), e);
         }catch (ParameterException e){
             if(jc != null) jc.usage();
-            System.out.println(e.getMessage());
-            //e.printStackTrace();
         }
     }
 }
