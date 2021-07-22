84ca0ddb9237e7c70cd0d6cf76db811c5bac5087
Nikola Bulat <nbulat99@gmail.com>
2021-07-22 15:47:27 +0200

    Implemented all the parameters for the report command. Everything needs to be tested.

--- src/main/java/cli/view_models/ReportCommand.java
+++ src/main/java/cli/view_models/ReportCommand.java
@@ -5,6 +5,9 @@ import com.beust.jcommander.Parameters;
 import data.Repositories;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Parameters(commandNames = {"report"},
             commandDescription = "Executing a report. Reports include the average prices for the given flags.")
@@ -21,18 +24,24 @@ public class ReportCommand {
     private String fuelType;
 
     @Parameter(names = "--petrol-station",
-               description = "The name of the petrol station for which the reports will be printed")
-    private String petrolStation;
+               description = "The name of the petrol station for which the reports will be printed",
+               variableArity = true)
+    private List<String> petrolStation = new ArrayList<>();
 
     @Parameter(names = "--city",
-               description = "The name of the city for which the reports will be printed")
-    private String city;
+               description = "The name of the city for which the reports will be printed",
+               variableArity = true)
+    private List<String> city = new ArrayList<>();
 
     public String getPeriod() {
         return period;
     }
 
+    private String getJoinedList(List<String> list){
+        return list.isEmpty() ? null : String.join(" ", list);
+    }
+
     public void report() throws SQLException {
-        Repositories.printTheReport(period, fuelType, petrolStation, city);
+        Repositories.printTheReport(period, fuelType, getJoinedList(petrolStation), getJoinedList(city));
     }
 }
