bfa5fe0d7eea4723b28fca5e983efc9c8a72b323
Nikola Bulat <nbulat99@gmail.com>
2021-07-23 13:07:01 +0200

    Reporting engine DateParameterValidator thread resolved

--- src/main/java/cli/view_models/DateParameterValidator.java
+++ src/main/java/cli/view_models/DateParameterValidator.java
@@ -13,24 +13,29 @@ public class DateParameterValidator implements IParameterValidator {
 
     @Override
     public void validate(String name, String value) throws ParameterException {
-        //DateTimeFormatterBuilder dtfBuilder = new DateTimeFormatterBuilder();
-        DateTimeFormatter yearMonthDayDtf = new DateTimeFormatterBuilder().appendPattern("yyyy-MM-dd").toFormatter();
-        DateTimeFormatter yearMonthDtf = new DateTimeFormatterBuilder().appendPattern("yyyy-MM").parseDefaulting(ChronoField.DAY_OF_MONTH, 1).toFormatter();
-        DateTimeFormatter yearDtf = new DateTimeFormatterBuilder().appendPattern("yyyy")
-                .parseDefaulting(ChronoField.MONTH_OF_YEAR, 1).parseDefaulting(ChronoField.DAY_OF_MONTH ,1).toFormatter();
+        DateTimeFormatter format = getFormat(value);
+        if (isValid(value, format)) return;
 
-        if(isValid(value, yearMonthDayDtf) || isValid(value, yearMonthDtf) || isValid(value, yearDtf)) return;
-
-        String message = String.format("The [%s] is not in a valid format.",  value);
+        String message = String.format("The [%s] is not in a valid format.", value);
         throw new ParameterException(message);
     }
 
-    private boolean isValid(String dateStr, DateTimeFormatter dtf){
+    private boolean isValid(String dateStr, DateTimeFormatter dtf) {
         try {
             LocalDate.parse(dateStr, dtf);
         } catch (DateTimeParseException e) {
             return false;
         }
         return true;
+    }
+
+    private DateTimeFormatter getFormat(String value) {
+        if (value.length() <= 4) return new DateTimeFormatterBuilder().appendPattern("yyyy")
+                .parseDefaulting(ChronoField.MONTH_OF_YEAR, 1).parseDefaulting(ChronoField.DAY_OF_MONTH, 1).toFormatter();
+
+        if (value.length() <= 7)
+            return new DateTimeFormatterBuilder().appendPattern("yyyy-MM").parseDefaulting(ChronoField.DAY_OF_MONTH, 1).toFormatter();
+
+        return new DateTimeFormatterBuilder().appendPattern("yyyy-MM-dd").toFormatter();
     }
 }
